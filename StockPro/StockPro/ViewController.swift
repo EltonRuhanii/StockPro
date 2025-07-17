7f707fd81940c1d4af154a9b38492daea06819de
WIZZARD <eltonruhani01@gmail.com>
2025-07-17 15:52:10 +0200

    Created an animation which animates a splashscreen

--- StockPro/StockPro/ViewController.swift
+++ StockPro/StockPro/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+            self.navigationController?.pushViewController(loginVC, animated: true)
+
+       }
     }
 
-
+  
 }
 
