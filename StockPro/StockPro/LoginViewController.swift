cac9028e21c318f41783ea2b0b05efbc4fb0981a
WIZZARD <eltonruhani01@gmail.com>
2025-07-17 15:51:46 +0200

    created login view controller and hid the backbutton

---
+++ StockPro/StockPro/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  StockPro
+//
+//  Created by WIZZARD on 17.7.25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
